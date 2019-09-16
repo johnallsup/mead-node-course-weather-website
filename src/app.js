0c3ea73ada434dd5c4b14dc27be5ea3a6665cd6e
John Chalisque Allsup <dev@chalisque.net>
2019-09-16 20:05:56 +0100

    Removed embarassing message from about page

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.get('',(req,res) => {
 })
 app.get('/about',(req,res) => {
     res.render('about',{
-        title: "About Mary's arse?",
+        title: "About this site.",
         name: "Bob the Ball"
     })
 })
